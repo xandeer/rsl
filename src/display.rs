e7a38a6599eda5987585c1d778af811f3c40e0db
Kevin Du <kkxandeer@gmail.com>
2020-03-22 15:16:22 +0800

    feat: display

---
+++ src/display.rs
@@ -0,0 +1,83 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let minmax = MinMax(0, 23);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}",
+    small = small_range,
+    big = big_range);
+
+    let point = Point2D { x: 3.3, y: 7.8 };
+
+    println!("Compare structures:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let comp = Complex { real: 3.3, imag: 8.9 };
+
+    println!("Compare structures:");
+    println!("Display: {}", comp);
+    println!("Debug: {:?}", comp);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
